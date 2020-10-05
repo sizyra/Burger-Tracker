a36aaf0f650a1a72c86560ebe5c40f4d844dfb35
Sam Zyra <sisaaczyra@gmail.com>
2020-10-05 10:49:41 -0700

    Built foundational elements for app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("baconator", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("double_baconator", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", false);
