a19177c9ddfcee66713715adf73823936428c816
Sam Zyra <sisaaczyra@gmail.com>
2020-10-07 13:13:55 -0700

    models first draft finished

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,26 @@
 const orm = require("../config/orm.js");
 
-class Burger {
-    constructor() {
-
-    };
-
-    getAll() {
-        orm.selectAll();
-    };
-
-    createNew() {
-        orm.insertOne();
-    };
-
-    update() {
-        orm.updateOne();
-    };
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+          cb(res);
+        });
+      },
+      update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+      },
+      delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+          cb(res);
+        });
+      }
 };
 
-modules.export = Burger;
+modules.export = burger;
