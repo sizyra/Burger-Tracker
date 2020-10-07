8413797c0d1afad096c18200cc143280d6d89876
Sam Zyra <sisaaczyra@gmail.com>
2020-10-07 13:35:45 -0700

    Fixed typo

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//const routes = require("./controllers/catsController.js");
+const routes = require("./controllers/burgers_controller.js");
 
-//app.use(routes);
+app.use(routes);
 
-//app.listen(PORT, function() {
-//  console.log("Server listening on: http://localhost:" + PORT);
-//});
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
