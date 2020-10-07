1d30ae304ad937a019489959c991cf0d1eae5c06
Sam Zyra <sisaaczyra@gmail.com>
2020-10-07 13:26:39 -0700

    Updated schema (burger_name>name), fin controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,51 @@ const burgerModel = require("../models/burger.js");
 const router = express.Router();
 
 router.get("/", function(req, res) {
+    burgerModel.all(function(data) {
+        var burgersObj = {
+            burgerModel: data
+        };
+        console.log(burgersObj);
+        res.render("index", burgersObj);
+    });
+});
 
-})
+router.post("/api/burgers", function(req,res) {
+    burgerModel.create([
+        "name"
+    ], [
+        req.body.name,
+    ], function(result) {
+        res.json({ id: result.insertID })
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burgerModel.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+  router.delete("/api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+  
+    burgerModel.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 modules.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ("baconator");
-INSERT INTO burgers (burger_name) VALUES ("double_baconator");
-INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
+INSERT INTO burgers (name) VALUES ("baconator");
+INSERT INTO burgers (name) VALUES ("double_baconator");
+INSERT INTO burgers (name) VALUES ("cheeseburger");
